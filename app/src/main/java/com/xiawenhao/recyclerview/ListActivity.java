4693bd5fca12dadb11e6cfeb23941d5127c81986
Jerry777777 <491818175@qq.com>
2020-08-17 15:51:23 +0800

    [xiawenhao] feat: define a model

--- app/src/main/java/com/xiawenhao/recyclerview/ListActivity.java
+++ app/src/main/java/com/xiawenhao/recyclerview/ListActivity.java
@@ -5,10 +5,42 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class ListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_listview);
+        setContentView(R.layout.activity_recyclerview);
+    }
+
+    public static class Contact {
+        private String mName;
+        private boolean mOnline;
+
+        public Contact(String name, boolean online) {
+            mName = name;
+            mOnline = online;
+        }
+
+        public String getName() {
+            return mName;
+        }
+
+        public boolean isOnline() {
+            return mOnline;
+        }
+
+        private static int lastContactId = 0;
+
+        public static ArrayList<Contact> createContactsList(int numContacts) {
+            ArrayList<Contact> contacts = new ArrayList<Contact>();
+
+            for (int i = 1; i <= numContacts; i++) {
+                contacts.add(new Contact("Person " + ++lastContactId, i <= numContacts / 2));
+            }
+
+            return contacts;
+        }
     }
 }
