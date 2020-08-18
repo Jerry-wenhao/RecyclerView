3dc96241e57a3fae201e3bea7b35f3d78370f32e
Jerry777777 <491818175@qq.com>
2020-08-18 19:15:36 +0800

    [xiawenhao] feat: add the header model

---
+++ app/src/main/java/com/xiawenhao/recyclerview/MultipleListActivity.java
@@ -0,0 +1,13 @@
+package com.xiawenhao.recyclerview;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MultipleListActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.header_multiple_list);
+    }
+}
