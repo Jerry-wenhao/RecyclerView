4c3431cc16691584c0ff938d058e15ffb1ab2b66
Jerry777777 <491818175@qq.com>
2020-08-18 22:22:52 +0800

    [xiawenhao] feat: creat the imageRecyclerView model

--- app/src/main/java/com/xiawenhao/recyclerview/ImageListActivity.java
+++ app/src/main/java/com/xiawenhao/recyclerview/ImageListActivity.java
@@ -1,4 +1,13 @@
 package com.xiawenhao.recyclerview;
 
-public class ImageListActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ImageListActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.image_list);
+    }
 }
